4cda9f372076819734540084e2b3c99a8238126e
openXu <openXu@yeah.net>
2017-03-17 14:08:51 +0800

    12345

--- app/src/main/java/com/openxu/svg/MainActivity.java
+++ app/src/main/java/com/openxu/svg/MainActivity.java
@@ -11,9 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+/*
         Drawable drawable = getResources().getDrawable(R.drawable.animatedvectordrawable);
-        if (drawable instanceof Animatable) { ((Animatable) drawable).start(); }
+        if (drawable instanceof Animatable) { ((Animatable) drawable).start(); }*/
     }
 }
